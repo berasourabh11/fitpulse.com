95733ec6e1d27986fb9397b30f36f35d151ab1a3
berasourabh11 <berasourabh2071@gmail.com>
2023-11-24 00:07:51 +0530

    Made the navbar responsive

--- client/src/scenes/navbar/link.tsx
+++ client/src/scenes/navbar/link.tsx
@@ -1,18 +1,22 @@
-// import AnchorLink from "react-anchor-link-smooth-scroll"
+import AnchorLink from "react-anchor-link-smooth-scroll"
+import { SelectedPage } from "../../shared/types";
 
-// type Props = {
-//     page: string;
-// }
+type Props = {
+    page: string;
+    selectedPage:string;
+    setSelectedPage:(value:SelectedPage)=> void;
+}
+ 
+const Link = ({page,selectedPage,setSelectedPage}: Props) => {
+    const lowerCasePage = page.toLowerCase().replace(/ /g,"") as SelectedPage;
+return (
+    <AnchorLink
+    className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""} transition duration-500 hover:text-primary-300`} 
+    href={`#${lowerCasePage}`} 
+    onClick={()=>setSelectedPage(lowerCasePage)}>
+        {page}
+    </AnchorLink>
+  )
+}
 
-// const Link = ({page}: Props) => {
-//   return (
-//     <AnchorLink
-//     className={} 
-//     href={} 
-//     onClick={}>
-//         {page}
-//     </AnchorLink>
-//   )
-// }
-
-// export default Link
+export default Link
