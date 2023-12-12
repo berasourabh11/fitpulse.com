d613a04eb2e859852297704b5d67e17cef3f5c44
berasourabh11 <berasourabh2071@gmail.com>
2023-12-13 01:58:49 +0530

    completed book session route

--- server/src/controller/booksessionController.ts
+++ server/src/controller/booksessionController.ts
@@ -3,6 +3,7 @@ import { convertTimeTo24HrFormat } from '../utils/convertTimeToSting';
 import activitiesModel from '../models/activitiesModel';
 import { IActivity } from '../utils/types';
 import { IBookedSession, bookedSessionModel } from '../models/bookedSessions';
+import { IUserData } from '../models/userDataModel';
 
 interface SessionTime {
     date: string;
@@ -38,17 +39,61 @@ export const bookSessionController = async (req: Request, res: Response) => {
             res.status(400).json({ message: 'Session not found' });
             return;
         }
-        const bookedSession = await bookedSessionModel.findOne({activityName,startTime});
-        if(bookedSession){
-            if(bookedSession.sessionUsers.length>=session.slots){
-                res.status(400).json({ message: 'Session is full' });
-                return;
-            }
+        // Check if the activity has not passed the current time
+        if(isToday(date) && checkifSessionPassed(date,startTime)){
+            res.status(400).json({ message: 'Session has already passed' });
+            return;
         }
 
+
+
+
+        let bookedSession:IBookedSession | null = await bookedSessionModel.findOne({activityName,activityDay:dayInWords.toLowerCase(),activityTime:startTime,activityDate:date});
+        if(!bookedSession){
+            bookedSession= new bookedSessionModel({activityName,activityId,activityDate:date,activityTime:startTime,activityDay:dayInWords.toLowerCase()});
+        }
+
+        // Check if the session is already booked
+        if(bookedSession.sessionUsers.includes((req as any).user.data._id)){
+            res.status(400).json({ message: 'Session already booked' });
+            return;
+        }
+
+        if(bookedSession.sessionUsers.length>=session.slots){
+            res.status(400).json({ message: 'No slots available' });
+            return;
+        }
+
+
+        bookedSession.sessionUsers.push((req as any).user.data._id);
+        await bookedSession.save();
         res.status(200).json({ message: 'Session booked successfully' });
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: 'Internal server error' });
     }
 };
+
+function checkifSessionPassed(date: string, startTime: string) {
+    const currentDate = new Date();
+    const sessionDate = new Date(date);
+    const sessionTime = new Date(startTime);
+    if (sessionDate < currentDate) {
+        return true;
+    }
+    if (sessionDate === currentDate) {
+        if (sessionTime < currentDate) {
+            return true;
+        }
+    }
+    return false;
+}
+function isToday(date: string) {
+    const currentDate = new Date();
+    const sessionDate = new Date(date);
+    return (
+        sessionDate.getDate() === currentDate.getDate() &&
+        sessionDate.getMonth() === currentDate.getMonth() &&
+        sessionDate.getFullYear() === currentDate.getFullYear()
+    );
+}
--- server/src/models/bookedSessions.ts
+++ server/src/models/bookedSessions.ts
@@ -1,18 +1,23 @@
 import mongoose, { Schema, Document } from 'mongoose';
-import { IUserData } from './userDataModel';
 
 interface IBookedSession extends Document {
     activityName: string;
     activityId: string;
     activityDate: Date;
-    sessionUsers: IUserData[];
+    sessionUsers: mongoose.Types.ObjectId[]; // Use mongoose.Types.ObjectId as the type
 }
 
 const bookedSessionSchema: Schema = new Schema({
     activityName: { type: String, required: true },
     activityId: { type: String, required: true },
-    activityDate: { type: Date, required: true },
-    sessionUsers: [{ type: Schema.Types.ObjectId, ref: 'Users' }]
+    activityDate: { type: String, required: true },
+    activityTime: { type: String, required: true },
+    activityDay: { type: String, required: true },
+    sessionUsers: {
+        type: [Schema.Types.ObjectId],
+        ref: 'users',
+        default: [], // Default value is an empty array
+    },
 });
 
 const bookedSessionModel = mongoose.model<IBookedSession>('BookedSessions', bookedSessionSchema);
