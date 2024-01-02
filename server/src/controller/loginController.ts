1ef13c866819d221ef529a11a1fc9d913aa6888c
berasourabh11 <berasourabh2071@gmail.com>
2024-01-02 21:28:19 +0530

    added samesite none

--- server/src/controller/loginController.ts
+++ server/src/controller/loginController.ts
@@ -11,7 +11,7 @@ export const loginController = async (req: Request, res: Response) => {
 
         // Handle the result and send a response
         if (verifiedUser) {
-            res.cookie('jwt', verifiedUser.token, { httpOnly: true,secure:true, expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7) });
+            res.cookie('jwt', verifiedUser.token, { httpOnly: true,secure:true, sameSite:'none', expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7) });
             res.status(200).json({ message: 'User verified', username: verifiedUser.username, email: verifiedUser.email, firstName: verifiedUser.firstName, lastName: verifiedUser.lastName});
         } else {
             res.status(401).json({ message: 'Invalid credentials' });
