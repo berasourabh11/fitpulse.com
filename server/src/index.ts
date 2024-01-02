708ce88e6753f388ead0c9f33049e92ea1966cd6
berasourabh11 <berasourabh2071@gmail.com>
2024-01-02 18:51:25 +0530

    pt 2

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,28 @@
 import express from 'express';
 import router from './routes';
-import cors from 'cors';
+import cors, { CorsOptions, CorsOptionsDelegate } from 'cors';
 import 'dotenv/config';
 import connectdb from './utils/connectdb';
 import cookieParser from 'cookie-parser';
 
 const app = express();
 
-const corsOptions = {
-    origin: 'https://fit-pulse-n4m4.onrender.com', 
-    credentials: true, 
+const allowedOrigins = ["https://fit-pulse-n4m4.onrender.com"];
+
+const corsOptions: cors.CorsOptions = {
+    origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'), false); 
+        }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200
 };
 
-app.use(cookieParser());
 app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
